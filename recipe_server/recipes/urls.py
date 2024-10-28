845ca014643e466fe45e9871b8767b07671a4295
Tapir79 <saara.m.pakarinen@gmail.com>
2024-10-28 19:22:51 +0200

    add recipe_detail

--- recipe_server/recipes/urls.py
+++ recipe_server/recipes/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('recipes', views.recipe_list, name='recipe_list'),
+    path('recipes/<int:recipe_id>/', views.recipe_detail, name='recipe_detail'), 
 ]
--- recipe_server/recipes/views.py
+++ recipe_server/recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import Recipe
 
 def index(request):
@@ -8,3 +8,7 @@ def recipe_list(request):
     recipes = Recipe.objects.all()  # Fetch all Recipe instances
     print(recipes)
     return render(request, 'recipe_list.html', {'recipes': recipes})
+
+def recipe_detail(request, recipe_id):
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    return render(request, 'recipe_detail.html', {'recipe': recipe})
